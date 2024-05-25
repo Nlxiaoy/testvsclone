343194522c7896cea83889d7f4c5874497a99741
Nlxiaoy <164542306+Nlxiaoy@users.noreply.github.com>
2024-05-26 00:47:15 +0800

    Add files via upload

---
+++ 二叉树的建立/二叉树的建立/二叉树的建立.cpp
@@ -0,0 +1,34 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct BiTNode {
+	int data;
+	struct BiTNode* lchild;
+	struct BiTNode* rchild;
+}BiTNode,*BiTree;
+
+bool Create_BiTree(BiTree* T) {
+	char  ch;
+	scanf_s("%c", &ch);
+	if (ch=='#')
+	{
+		*T = NULL;
+		return true;
+	}
+	else
+	{
+		*T = (BiTNode*)malloc(sizeof(BiTNode));
+		(*T)->data = ch;
+		Create_BiTree(&(*T)->lchild);
+		Create_BiTree(&(*T)->rchild);
+	}
+	return true;
+}
+
+int main()
+{
+	BiTree T;
+	Create_BiTree(&T);
+	return 0;
+}
